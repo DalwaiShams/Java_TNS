780732d69e84104ea48fa4cb1f286a30c7597a8e
Shams Dalwai <dalwaishams1@gmail.com>
2024-06-27 21:03:52 +0530

    commit
    committ

---
+++ assignment2/src/com/shams/assignmentwo/Main.java
@@ -0,0 +1,13 @@
+package com.shams.assignmentwo;
+
+public class Main {
+	public static void main(String[] args) {
+		Student obj = new Student();
+		obj.doSomething(); 
+		
+		Commission object = new Commission();
+		object.acceptDetails();
+		object.displayDetails();
+		object.calculateCommission();
+	}
+}
---
+++ assignment2/src/com/shams/assignmentwo/Student.java
@@ -0,0 +1,12 @@
+package com.shams.assignmentwo;
+
+public class Student {
+	// Default constructor
+	Student() {
+		System.out.println("Student object is created");
+	}
+	
+	public void doSomething() {
+		System.out.println("Doing something with the Student object.");
+	}
+}
