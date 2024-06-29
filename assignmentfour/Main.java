7d1d607fa3fdaaf19403262fb21b8f330a661a6c
Shams Dalwai <dalwaishams1@gmail.com>
2024-06-29 11:49:48 +0530

    commit
    initial commit

---
+++ assignmentfour/Main.java
@@ -0,0 +1,36 @@
+package com.shams.javassignments.assignmentfour;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	TicketReservation obj=new TicketReservation();
+	try (Scanner sc = new Scanner(System.in)) {
+		while (true) {
+		    System.out.println("Total tickets available is:" + TicketReservation.numOfTicktes + "\nTo book ticket enter the below details:");
+		    obj.acceptDetails();
+		    System.out.println(obj);
+		    TicketReservation.calcTickets();
+		    System.out.println("Do you want to book more tickets? Enter Yes or No");
+		    String answer = sc.nextLine().toLowerCase();
+
+		    if (answer.equals("yes")) {
+		        continue;
+		    } else if (answer.equals("no")) {
+		        break;
+		    } else {
+		        while (!answer.equals("yes") && !answer.equals("no")) {
+		            System.out.println("Please give the correct input (Yes or No)");
+		            answer = sc.nextLine().toLowerCase();
+		        }
+		        if (answer.equals("yes")) {
+		            continue;
+		        } else {
+		            break;
+		        }
+		    }
+		}
+	}
+}
+}
